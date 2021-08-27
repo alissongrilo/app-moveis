045a56387c00f5e61b8872b1bd03b64235b481cf
alissongrilo <alissonmiguel197@gmail.com>
2021-08-27 15:57:20 -0300

    Running v2.2 on 27/08

--- src/routes/drawer.routes.tsx
+++ src/routes/drawer.routes.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { Usuario, Cadastrar } from "../pages";
+import { Usuario, Cadastrar, Camera } from "../pages";
+import { AntDesign, FontAwesome5  } from '@expo/vector-icons'; 
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Usuario" component={Usuario} />
-      <Drawer.Screen name="Cadastrar" component={Cadastrar} />
+    <Drawer.Navigator screenOptions={{
+      headerShown: true}}>
+
+      <Drawer.Screen name="Usuario" component={Usuario} options={{
+        drawerIcon: () => <AntDesign name="user" size={24} color="black" />}}/>
+
+      <Drawer.Screen name="Cadastrar" component={Cadastrar} options={{
+        drawerIcon: () => <FontAwesome5 name="cash-register" size={24} color="black" />}}/>
+
+      <Drawer.Screen name="Camera" component={Camera} options={{
+        drawerIcon: () => <AntDesign name="camera" size={24} color="black" />}}/>
     </Drawer.Navigator>
   );
 }
